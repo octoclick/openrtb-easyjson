a86b7568b5d74ee27351e2d82540f7aa2d05860d
Artem Kiselev <kiselev@gismeteo.ru>
2024-04-16 14:11:19 +0300

    add CB-2823

--- native/response/response.go
+++ native/response/response.go
@@ -8,10 +8,11 @@ import (
 
 // Response is the native object is the top level JSON object which identifies a native response.
 type Response struct {
-	Version     openrtb.StringOrNumber `json:"ver,omitempty"`         // Version of the Native Markup
-	Assets      []Asset                `json:"assets"`                // An array of Asset Objects
-	Link        Link                   `json:"link"`                  // Destination Link. This is default link object for the ad
-	ImpTrackers []string               `json:"imptrackers,omitempty"` // Array of impression tracking URLs, expected to return a 1x1 image or 204 response
-	JSTracker   string                 `json:"jstracker,omitempty"`   // Optional JavaScript impression tracker. This is a valid HTML, Javascript is already wrapped in <script> tags. It should be executed at impression time where it can be supported
-	Ext         json.RawMessage        `json:"ext,omitempty"`
+	Version       openrtb.StringOrNumber `json:"ver,omitempty"`           // Version of the Native Markup
+	Assets        []Asset                `json:"assets"`                  // An array of Asset Objects
+	Link          Link                   `json:"link"`                    // Destination Link. This is default link object for the ad
+	ImpTrackers   []string               `json:"imptrackers,omitempty"`   // Array of impression tracking URLs, expected to return a 1x1 image or 204 response
+	JSTracker     string                 `json:"jstracker,omitempty"`     // Optional JavaScript impression tracker. This is a valid HTML, Javascript is already wrapped in <script> tags. It should be executed at impression time where it can be supported
+	EventTrackers []EventTrackers        `json:"eventtrackers,omitempty"` // Array of tracking objects to run with the ad, in response to the declared supported methods in the request. Replaces imptrackers and jstracker, to be deprecated.
+	Ext           json.RawMessage        `json:"ext,omitempty"`
 }
